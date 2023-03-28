4d2a650d02a6bf5981a1566fb7cd7826289d8e96
granthendrickson <122942799+granthendrickson@users.noreply.github.com>
2023-03-27 22:57:19 -0400

    added more plantbase UI

--- src/pages/plantbase.js
+++ src/pages/plantbase.js
@@ -1,18 +1,44 @@
 import Pin from '../Components/Pin';
+import { IconButton } from "rsuite";
+import { Plus, Copy, CheckOutline } from '@rsuite/icons';
+
 
 export default function PlantBase() {
-    return <main>
-        <Pin pinSize = {'small'} />
-        <Pin pinSize = {'medium'} />
-        <Pin pinSize = {'large'} />
-        <Pin pinSize = {'small'} />
-        <Pin pinSize = {'medium'} />
-        <Pin pinSize = {'large'} />
-        <Pin pinSize = {'small'} />
-        <Pin pinSize = {'medium'} />
-        <Pin pinSize = {'large'} />
-        <Pin pinSize = {'small'} />
-        <Pin pinSize = {'medium'} />
-        <Pin pinSize = {'large'} />
-  </main>
+    return <div className='plantbase-page'>
+      <h1 id='your-plantbase'>Your Plantbase:</h1>
+
+      <div className='plantbase-button-container'>
+        <div className='plantbase-button'>
+          <IconButton className='plantbase-icon' icon={ <Plus /> }appearance='primary' />
+          <p>Add Plant</p>
+        </div>
+        <div className='plantbase-button'>
+          <IconButton className='plantbase-icon' icon={ <Copy /> }appearance='primary' />
+          <p>Organize</p>
+        </div>
+        <div className='plantbase-button'>
+        <IconButton className='plantbase-icon' icon={ <CheckOutline /> }appearance='primary' />
+        <p>To-Dos</p>
+        </div>
+      </div>
+
+      <div className='plantbase'>
+        <h1 id="number-of-plants">12 Plants</h1>
+
+        <div className='plant-container'>
+          <Pin pinSize = {'small'} />
+          <Pin pinSize = {'medium'} />
+          <Pin pinSize = {'large'} />
+          <Pin pinSize = {'small'} />
+          <Pin pinSize = {'medium'} />
+          <Pin pinSize = {'large'} />
+          <Pin pinSize = {'small'} />
+          <Pin pinSize = {'medium'} />
+          <Pin pinSize = {'large'} />
+          <Pin pinSize = {'small'} />
+          <Pin pinSize = {'medium'} />
+          <Pin pinSize = {'large'} />
+        </div>
+      </div>
+  </div>
 }
