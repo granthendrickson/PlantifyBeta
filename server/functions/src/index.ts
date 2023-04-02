422502393c5a095a77aaac7a816a8a0dfb71504c
Nick <nickgr1052@gmail.com>
2023-04-02 02:26:13 -0400

    Added register function to cloud. This is the code

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,18 +1,40 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions"
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const register = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true})
-  response.send("Hello from Firebase!")
+export const registerUser = functions.https.onRequest(async (request, response) => {
+  
+  const {firstName, lastName, email, username, password} = request.body
+
+  try {
+    const userRecord = await admin.auth().createUser({
+      email: email,
+      password: password,
+      displayName: username
+    })
+
+    await admin.firestore().collection('users').doc(userRecord.uid).set({
+      firstName,
+      lastName,
+    })
+
+    response.json({message: 'User registered successfully', userId: userRecord.uid})
+  }
+  catch(error){
+    response.status(500).json({message: "Unable to register user", errors: error })
+  }
+  
 });
 
 
 
-export const sign_in = functions.https.onRequest((request, response) => {
+// export const sign_in = functions.https.onRequest((request, response) => {
     
-});
+// });
 
 // BASIC CRUD (Create - Read - Update - Delete)
 // Create
@@ -20,16 +42,16 @@ export const add_plant = functions.https.onRequest((request, response) => {
     
 });
 
-// READ
-export const search_users_or_plants = functions.https.onRequest((request, response) => {
+// // READ
+// export const search_users_or_plants = functions.https.onRequest((request, response) => {
     
-});
+// });
 
-export const edit_plant = functions.https.onRequest((request, response) => {
+// export const edit_plant = functions.https.onRequest((request, response) => {
 
-});
+// });
 
-// DELETE
-export const del_plant = functions.https.onRequest((request, response) => {
+// // DELETE
+// export const del_plant = functions.https.onRequest((request, response) => {
     
-});
+// });
