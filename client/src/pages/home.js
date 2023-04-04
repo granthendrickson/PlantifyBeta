28734567f07334031f4fd7beb323020b0377b7c0
Nick <nick1052@Knights.ucf.edu>
2023-04-04 16:37:31 -0400

    Old Server files

--- client/src/pages/home.js
+++
@@ -1,7 +0,0 @@
-import monsteraimg from "../img/Monstera.png"
-
-export default function Home() {
-    return <div className="home">
-        <img className='background-plant' src={ monsteraimg } alt="" />
-    </div>
-}
